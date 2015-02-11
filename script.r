aeb76d269b7997034217935486aca8fe0b8a9793
JonMinton <nate.minton@gmail.com>
2015-02-11 12:39:29 +0000

    first commit

---
+++ script.r
@@ -0,0 +1,19 @@
+# First script
+# 11/2/2015
+
+
+# To do : 
+# 1) Send Poverty & Suburbs work to Ade
+# 2) reply to emails
+# 3) extract list of dzs as per Ellie's suggestion
+# 4) add packrat to see if this solves markdown issue
+
+rm(list=ls())
+
+
+require(repmis)
+require(plyr)
+require(tidyr)
+require(dplyr)
+require(rmarkdown)
+
