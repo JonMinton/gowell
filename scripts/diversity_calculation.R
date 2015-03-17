8654b9dc5bb9c941bbe2415c68e5d27e6fe1e5f4
Jonathan Minton <nate.minton@gmail.com>
2015-03-17 12:02:01 +0000

    add more 2011 census tables; add H (entropy) function to diversity calculation script

--- scripts/diversity_calculation.R
+++ scripts/diversity_calculation.R
@@ -18,7 +18,15 @@ require(dplyr)
 require(vegan)
 require(ggplot2)
 
+# Functions 
 
+# Entropy calculation
+H <- function(xx){
+    N <- ncol(xx)
+    p <- apply(xx, 1, function(x) (x+0.5)/sum(x))
+    out <- -1 * apply(p, 2, function(x) sum(x * log(x))/N)
+    return(out)
+}
 # This section will use relevant variables from the 2011 Census to identify the 
 # mix of occupational classes in each datazone
 
@@ -92,8 +100,14 @@ tmp$diversity <- tmp[,-1] %>%
     as.matrix %>%
     diversity 
 
+tmp$H <- tmp  %>% 
+    ungroup  %>% 
+    select(-datazone, -diversity)  %>%
+    as.matrix %>%
+    H
+
 dz_sec_diversity <- tmp %>%
-    select(datazone, sec_div=diversity)
+    select(datazone, sec_div=diversity, sec_h=H)
 dz_sec_diversity
 
 write.csv(dz_sec_diversity, file="data/derived/diversity_sec_by_dz_2011_census.csv")
@@ -139,11 +153,20 @@ tenure_households$diversity <- tenure_households %>%
     as.matrix %>%
     diversity
 
+tenure_households$H <- tenure_households  %>% 
+    ungroup  %>% 
+    select(-datazone, -diversity)  %>%
+    as.matrix %>%
+    H
+
+tenure_households <- tenure_households %>%
+    select(datazone, diversity, H)
+
 write.csv(tenure_households, file="data/derived/diversity_tenure_2001.csv", row.names=FALSE)
 
 # 6505 observations - whole of Scotland
 # left join to just Greater Glasgow
-tenure_households <- greater_glasgow_dzs %>% left_join(tenure_households)
+# tenure_households <- greater_glasgow_dzs %>% left_join(tenure_households)
 # now 2200 observations
 # this is 34% of total, dzs are approx equal population
 # if Scot population is 5.3 million this implies 
@@ -161,16 +184,18 @@ dwelling_bands <- read.csv("data/derived/dwellings_by_band.csv") %>%
 
 dwelling_bands
 # going for 2011 for comparability with census
-tmp <- dwelling_bands %>%
-    filter(year==2011) %>%
-    select(-1, -2) %>%
+dwelling_bands_2011 <- dwelling_bands %>%
+    filter(year==2011)
+
+dwelling_bands_2011$diversity <- dwelling_bands_2011%>%
+    select(-datazone, -year) %>%
     as.matrix %>%
     diversity
 
-dwelling_diversity_2011 <- dwelling_bands  %>%
-    filter(year==2011) %>%
-    select(datazone) %>%
-    mutate(diversity=tmp)
+dwelling_bands_2011$H <- dwelling_bands_2011%>%
+    select(-datazone, -year, -diversity) %>%
+    as.matrix %>%
+    H
 
 write.csv(dwelling_diversity_2011, file="data/derived/diversity_dwelling_band_2011.csv", row.names=FALSE)
 
@@ -189,8 +214,13 @@ tmp$diversity <- tmp %>%
     as.matrix %>%
     diversity 
 
+tmp$H <- tmp %>%
+    select(-datazone, diversity) %>%
+    as.matrix %>%
+    H 
+
 dwelling_type_diversity_2011 <- tmp %>%
-    select(datazone, diversity)
+    select(datazone, diversity, H)
 
 write.csv(dwelling_type_diversity_2011, file="data/derived/diversity_dwelling_type_2011.csv", row.names=FALSE)
 
@@ -209,10 +239,19 @@ tmp <- household_spaces %>%
     ) %>% 
     select(datazone, total, occupied, holiday, vacant) 
 
-tmp$diversity <- tmp  %>% select(-1, -2)  %>% as.matrix  %>% diversity  
+tmp$diversity <- tmp  %>% 
+    select(-datazone, -total)  %>% 
+    as.matrix  %>% 
+    diversity  
+
+tmp$H <- tmp  %>% 
+    select(-datazone, -total, -diversity)  %>% 
+    as.matrix  %>% 
+    H  
+
 
 space_diversity_2001 <- tmp  %>%
-    select(datazone, diversity)
+    select(datazone, diversity, H)
 rm(tmp)
 write.csv(space_diversity_2001, file="data/derived/diversity_space_2001.csv", row.names=FALSE)
 
@@ -225,32 +264,40 @@ eth_2011 <- eth_2011 %>%
     select(datazone, type,  count)
 
 eth_2011 <- eth_2011 %>%
-    mutate(count = str_replace_all(count, "-", 0))
+    mutate(count = str_replace_all(str_replace_all(count, "-", "0"), ",", ""),
+           count= as.numeric(as.character(count)))
 
-eth_2011$count <- eth_2011$count %>%
-    as.character %>%
-    as.numeric
+
+# african 
+# african..african..african.scottish.or.african.british 
+# african..other.african 
+# all.people 
+# asian..asian.scottish.or.asian.british 
+# asian..asian.scottish.or.asian.british..bangladeshi..bangladeshi.scottish.or.bangladeshi.british 
+# asian..asian.scottish.or.asian.british..chinese..chinese.scottish.or.chinese.british 
+# asian..asian.scottish.or.asian.british..indian..indian.scottish.or.indian.british 
+# asian..asian.scottish.or.asian.british..other.asian 
+# asian..asian.scottish.or.asian.british..pakistani..pakistani.scottish.or.pakistani.british 
+# caribbean.or.black 
+# caribbean.or.black..black..black.scottish.or.black.british  
+# caribbean.or.black..caribbean..caribbean.scottish.or.caribbean.british 
+# caribbean.or.black..other.caribbean.or.black 
+# mixed.or.multiple.ethnic.groups 
+# other.ethnic.groups 
+# other.ethnic.groups..arab..arab.scottish.or.arab.british 
+# other.ethnic.groups..other.ethnic.group 
+# white 
+# white..gypsy.traveller
+# white..irish 
+# white..other.british 
+# white..other.white 
+# white..polish 
+# white..scottish 
 
 eth_2011 %>%
     group_by(type) %>%
-    summarise %>%
-    print(n=30)
-
-
-
-# type
-# 1    african
-# 4    all.people
-# 5	asian..asian.scottish.or.asian.british
-# 11	caribbean.or.black
-# 15	mixed.or.multiple.ethnic.groups
-# 16	other.ethnic.groups
-# 20	white..gypsy.traveller
-# 21	white..irish
-# 22	white..other.british
-# 23	white..other.white
-# 24	white..polish
-# 25	white..scottish
+    tally %>%
+    print(n=25)
 
 tmp <- eth_2011 %>%
     spread(type, count) %>%
@@ -271,8 +318,12 @@ tmp$diversity <- tmp  %>%
     as.matrix %>%
     diversity 
 
+tmp$H <- tmp %>%
+    select(-datazone, -diversity) %>%
+    H
+
 diversity_eth_2011 <- tmp %>%
-    select(datazone, diversity)
+    select(datazone, diversity, H)
 
 write.csv(diversity_eth_2011, file="data/derived/diversity_ethnicity_2011.csv", row.names=FALSE)
 
@@ -309,8 +360,13 @@ tmp$diversity <- tmp %>%
     as.matrix %>%
     diversity
 
+tmp$H <- tmp %>%
+    select(-datazone, diversity) %>%
+    as.matrix %>%
+    H
+
 diversity_demo_2010 <- tmp %>%
-    select(datazone, diversity)
+    select(datazone, diversity, H)
 
 write.csv(diversity_demo_2010, file="data/derived/demographic_diversity_2010.csv", row.names=FALSE)
 
@@ -319,4 +375,166 @@ write.csv(diversity_demo_2010, file="data/derived/demographic_diversity_2010.csv
 # DONE
 
 
+#### Additional sources of diversity?
+
+# Candidates from SNS
+
+
+# 2011 Census Tables ------------------------------------------------------
+
+
+# The following tables have been found in the 2011 Census
+
+# KS 204 SC     Country of birth [already done]
+# QS 203 SC     Country of birth [already done]
+# KS 501 SC     Highest qualification
+# KS 605 SC     Industry
+# QS 605 SC     Industry
+# KS 206 SC     Language
+# QS 702 SC     Method of Travel to Work
+# KS 202 SC     National Identity
+# KS 608 SC     Occupation
+# QS 606 SC     Occupation
+# KS 209 SCb    Religion
+# KS 209 SCa    Religion (UK Harmonised)
+# QS 101 SC     Residence Type
+# KS 402 SC     Tenure
+
+
+
+
+rm(list=ls())
+
+require(repmis)
+require(tidyr)
+require(stringr)
+require(plyr)
+require(dplyr)
+require(vegan)
+require(ggplot2)
+
+hq <- read.csv("data/2011_census/KS501SC.csv") %>%
+    tbl_df
+
+hq <- hq %>%
+    slice(-1) %>%
+    rename(datazone=X) %>%
+    gather(key=key, value=count, -datazone) %>%
+    mutate(count=as.numeric(str_replace_all(str_replace_all(count, ",", ""), "-", "0")))
+
+hq %>%
+    group_by(key) %>%
+    tally
+
+hq <- hq %>%
+    mutate(
+        key=mapvalues(
+            key,
+            from= c(
+                "All.people.aged.16.and.over",
+                "All.people.aged.16.and.over..No.qualifications",
+                "All.people.aged.16.and.over..Highest.level.of.qualification..Level.1.qualifications",
+                "All.people.aged.16.and.over..Highest.level.of.qualification..Level.2.qualifications",
+                "All.people.aged.16.and.over..Highest.level.of.qualification..Level.3.qualifications",
+                "All.people.aged.16.and.over..Highest.level.of.qualification..Level.4.qualifications.and.above",
+                "Schoolchildren.and.full.time.students..Aged.16.to.17",
+                "Schoolchildren.and.full.time.students..Aged.18.and.over",
+                "Full.time.students.aged.18.to.74..Economically.active..In.employment",
+                "Full.time.students.aged.18.to.74..Economically.active..Unemployed",
+                "Full.time.students.aged.18.to.74..Economically.Inactive"
+                ),
+            to= c(
+                NA,
+                "none",
+                "lvl_1",
+                "lvl_2",
+                "lvl_3",
+                "lvl_4",
+                NA,
+                NA,
+                NA,
+                NA,
+                NA
+                )
+                     
+            )
+        
+        ) %>%
+    filter(!is.na(key)) %>%
+    spread(key, count)
+
+
+hq$diversity <- hq  %>% 
+    select(-1)  %>% 
+    as.matrix %>%
+    diversity 
+    
+hq <- hq %>%
+    select(datazone, diversity)
+
+write.csv(hq, file="data/derived/highest_qual_diversity_2011.csv", row.names=FALSE)
+
+# Industry
+
+in1 <- read.csv("data/2011_census/KS605SC.csv") %>%
+    tbl_df
+
+in1 <- in1 %>%
+    slice(-1) %>%
+    rename(datazone=X) %>%
+    gather(key=key, value=count, -datazone) %>%
+    mutate(count=as.numeric(str_replace_all(str_replace_all(count, ",", ""), "-", "0")))
+
+in1 %>%
+    group_by(key) %>%
+    tally
+
+in1 <- in1 %>%
+    mutate(
+        key=str_replace_all(str_extract(key, "^[A-Z]{1}[\\.]{2}"), "\\.", "")) %>%
+    filter(!is.na(key)) %>%
+    spread(key, count)
+
+in1$diversity <- in1  %>% 
+    select(-1)  %>% 
+    as.matrix %>%
+    diversity 
+
+in1 <- in1 %>%
+    select(datazone, diversity)
+
+write.csv(in1, file="data/derived/industry_1_diversity_2011.csv", row.names=FALSE)
+
+
+## Language
+
+
+lan <- read.csv("data/2011_census/KS206SC.csv") %>%
+    tbl_df
+
+lan <- lan %>%
+    slice(-1) %>%
+    rename(datazone=X) %>%
+    gather(key=key, value=count, -datazone) %>%
+    mutate(count=as.numeric(str_replace_all(str_replace_all(count, ",", ""), "-", "0")))
+
+lan %>%
+    group_by(key) %>%
+    tally
+
+lan in1 <- in1 %>%
+    mutate(
+        key=str_replace_all(str_extract(key, "^[A-Z]{1}[\\.]{2}"), "\\.", "")) %>%
+    filter(!is.na(key)) %>%
+    spread(key, count)
+
+in1$diversity <- in1  %>% 
+    select(-1)  %>% 
+    as.matrix %>%
+    diversity 
+
+in1 <- in1 %>%
+    select(datazone, diversity)
+
+write.csv(in1, file="data/derived/industry_1_diversity_2011.csv", row.names=FALSE)
 
