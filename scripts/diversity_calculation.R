e4e7d535fc7c547ffb82881ddd60b7ffa86d1681
Jonathan Minton <nate.minton@gmail.com>
2015-05-01 17:38:14 +0100

    calc H and S, and show and tabulate correlations and PCA loadings for both measures

--- scripts/diversity_calculation.R
+++ scripts/diversity_calculation.R
@@ -11,6 +11,7 @@ require(plyr)
 require(dplyr)
 require(vegan)
 require(ggplot2)
+require(xtable)
 
 # Entropy function --------------------------------------------------------
 
@@ -187,7 +188,6 @@ all_H <- tenure_H %>%
     full_join(land_bus_H) %>%
     rename(land_bus=H)
 
-# quick regression
 
 
 greater_glasgow_dzs <- read.csv("data/geographies/dzs_in_greater_glasgow.csv") %>% 
@@ -204,6 +204,11 @@ all_H <- all_H %>%
     filter(!is.na(period)) %>%
     select(datazone, year, period, type, H) %>%
     spread(key=type, value=H)
+
+
+
+# H, Correlations in both periods --------------------------------------------
+
 
 p_all_H <- all_H %>% 
     gather(key=type, value=H, -datazone, -year, -period)  %>% 
@@ -223,116 +228,374 @@ fn <- function(x){
 H_corrs  <- dlply(p_all_H, .(period), fn)
 
 
+# Table of correlations, t1 -----------------------------------------------
 
-# Factor analysis ---------------------------------------------------------
+tab <- H_corrs[["t1"]]
+print(xtable(tab), type="html", file="tables/Correlations_between_entropy_levels_t1.html")
 
-# prcomp
+# Table of correlations, t2 -----------------------------------------------
+tab <- H_corrs[["t2"]]
+print(xtable(tab), type="html", file="tables/Correlations_between_entropy_levels_t2.html")
 
-fit <- p_all_H %>%
+
+
+# PCA, around 2001 --------------------------------------------------------
+
+fit_t1 <- p_all_H %>%
     filter(period=="t1") %>%
     filter(complete.cases(.)) %>%
     select(-datazone, -period) %>%
     prcomp(., cor=T)
 
-summary(fit)
-plot(fit)
 
-fit <- p_all_H %>%
+
+# table of H loadings around 2001 -----------------------------------------
+
+fit_t1$rotation  %>% 
+    round(2)  %>%
+    xtable %>%
+    print(type="html", file="tables/entropy_factor_loadings_t1.html")
+     
+
+# Graph of H loadings around 2001 -----------------------------------------
+
+fit_t1$rotation  %>% 
+    round(2)  %>% 
+    as.data.frame  %>%
+    mutate(type=rownames(.))  %>% 
+    gather(key="pc", value="loading", -type)  %>% 
+    ggplot(data=.) + 
+    geom_point(aes(x=loading, y= type)) + 
+    facet_wrap( ~ pc) + 
+    geom_segment(
+        aes(
+            y=type, yend=type, 
+            x=ifelse(loading < 0, loading, 0), 
+            xend=ifelse(loading > 0, loading, 0)
+            )
+        ) + 
+    labs(x="Factor loading on Principal Component", y="Entropy Type", title="Factor loadings around 2001") +
+    theme(plot.title=element_text(face = "bold"))
+ggsave("figures/h_factor_loadings_pca_around_2001.tiff", dpi=300, height=30, width=30, unit="cm")
+
+
+
+# pca around 2011 ---------------------------------------------------------
+
+fit_t2 <- p_all_H %>%
+    filter(period=="t2") %>%
+    filter(complete.cases(.)) %>%
+    select(-datazone, -period) %>%
+    prcomp(., cor=T)
+
+
+
+# table of H loadings around 2011 -----------------------------------------
+
+
+fit_t2$rotation  %>% 
+    round(2)  %>%
+    xtable %>%
+    print(type="html", file="tables/entropy_factor_loadings_t2.html")
+
+print(xtable(tab), type="html", file="tables/e0_mean_1750_onwards.html")
+
+
+
+# Graph of H loadings around 2001 -----------------------------------------
+
+fit_t2$rotation  %>% 
+    round(2)  %>% 
+    as.data.frame  %>%
+    mutate(type=rownames(.))  %>% 
+    gather(key="pc", value="loading", -type)  %>% 
+    ggplot(data=.) + 
+    geom_point(aes(x=loading, y= type)) + 
+    facet_wrap( ~ pc) + 
+    geom_segment(
+        aes(
+            y=type, yend=type, 
+            x=ifelse(loading < 0, loading, 0), 
+            xend=ifelse(loading > 0, loading, 0)
+        )
+    ) + 
+    labs(x="Factor loading on Principal Component", y="Entropy Type", title="Factor loadings around 2011") +
+    theme(plot.title=element_text(face = "bold"))
+ggsave("figures/h_factor_loadings_pca_around_2011.tiff", dpi=300, height=30, width=30, unit="cm")
+
+
+
+# Do as above, but Shannon's diversity index not H ----------------------------------------
+
+tenure$S <- diversity(as.matrix(tenure[,c("social", "rented", "owned")]))
+tenure_S <- tenure %>%
+    select(datazone=dz_2001, year=year, S=S)
+
+
+bld_band$S <- bld_band  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+bld_band_S <- bld_band %>%
+    select(datazone, year, S)
+
+bld_size$S <- bld_size  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+    
+bld_size_S <- bld_size %>%
+    select(datazone, year, S)
+
+bld_type$S <- bld_size  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+bld_type_S <- bld_type %>%
+    select(datazone, year, S)
+
+demo_as$S <- demo_as  %>% 
+    select(-dz_2001, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+demo_as_S <- demo_as %>%
+    select(datazone=dz_2001, year, S)
+
+
+demo_eth$S <- demo_eth  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+demo_eth_S <- demo_eth %>%
+    select(datazone, year, S)
+
+demo_rel$S <- demo_rel  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+demo_rel_S <- demo_rel %>%
+    select(datazone, year, S)
+
+demo_coo$S <- demo_coo  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+demo_coo_S <- demo_coo %>%
+    select(datazone, year, S)
+
+
+econ_qual$S <- econ_qual  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+    
+econ_qual_S <- econ_qual %>%
+    select(datazone, year, S)
+
+econ_act$S <- econ_act  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+econ_act_S <- econ_act %>%
+    select(datazone, year, S)
+
+econ_sec$S <- econ_sec  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+econ_sec_S <- econ_sec %>%
+    select(datazone, year, S)
+
+econ_ind$S <- econ_ind  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+econ_ind_S <- econ_ind %>%
+    select(datazone, year, S)
+
+land_vacant$S <- land_vacant  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+    
+land_vacant_S <- land_vacant %>%
+    select(datazone, year, S)
+
+
+land_bus$S <- land_bus  %>% 
+    select(-datazone, -year, -H)  %>% 
+    as.matrix  %>% 
+    diversity
+
+land_bus_S <- land_bus %>%
+    select(datazone, year, S)
+
+# Now to combine where there are common years
+
+all_S <- tenure_S %>%
+    rename(tenure=S) %>%
+    full_join(bld_band_S) %>%
+    rename(bld_band=S) %>%
+    full_join(bld_size_S) %>%
+    rename(bld_size=S) %>%
+    full_join(bld_type_S) %>%
+    rename(bld_type=S) %>%
+    full_join(demo_as_S) %>%
+    rename(demo_as=S) %>%
+    full_join(demo_eth_S) %>%
+    rename(demo_eth=S) %>%
+    full_join(demo_rel_S)  %>% 
+    rename(demo_rel=S) %>%
+    full_join(demo_coo_S) %>%
+    rename(demo_coo=S) %>%
+    full_join(econ_qual_S) %>%
+    rename(econ_qual=S) %>%
+    full_join(econ_act_S) %>%
+    rename(econ_act=S) %>%
+    full_join(econ_sec_S) %>%
+    rename(econ_sec=S) %>%
+    full_join(econ_ind_S) %>%
+    rename(econ_ind=S) %>%
+    full_join(land_vacant_S) %>%
+    rename(land_vacant=S) %>%
+    full_join(land_bus_S) %>%
+    rename(land_bus=S)
+
+
+
+greater_glasgow_dzs <- read.csv("data/geographies/dzs_in_greater_glasgow.csv") %>% 
+    tbl_df() %>%
+    rename(datazone=dz_2001)
+
+all_S <- all_S %>%
+    gather(key=type, value=S, -datazone, -year) %>%
+    filter(!is.na(S) & !is.na(year)) %>%
+    group_by(type) %>%
+    mutate(t1 = min(year), t2=max(year)) %>%
+    mutate(d1 = abs(year - t1), d2 = abs(year - t2)) %>%
+    mutate(period = ifelse(d1==min(d1), "t1", ifelse(d2==min(d2), "t2", NA))) %>%
+    filter(!is.na(period)) %>%
+    select(datazone, year, period, type, S) %>%
+    spread(key=type, value=S)
+
+
+
+
+# S, Correlations in both periods --------------------------------------------
+
+
+p_all_S <- all_S %>% 
+    gather(key=type, value=S, -datazone, -year, -period)  %>% 
+    filter(!is.na(S) & is.finite(S))  %>% 
+    filter(datazone %in% greater_glasgow_dzs$datazone) %>% 
+    select(-year)  %>% 
+    spread(key=type, value=S)
+
+fn <- function(x){
+    xx <- as.matrix(x[,-c(1,2)])
+    out <- cor(xx, use="pairwise.complete.obs") %>%
+        round(2)
+    
+    return(out)
+}
+
+S_corrs  <- dlply(p_all_S, .(period), fn)
+
+
+# Table of correlations, t1 -----------------------------------------------
+
+tab <- S_corrs[["t1"]]
+print(xtable(tab), type="html", file="tables/Correlations_between_shannon_diversity_t1.html")
+
+# Table of correlations, t2 -----------------------------------------------
+tab <- S_corrs[["t2"]]
+print(xtable(tab), type="html", file="tables/Correlations_between_shannon_diversity_t2.html")
+
+
+
+# S, PCA, around 2001 --------------------------------------------------------
+
+fit_t1 <- p_all_S %>%
     filter(period=="t1") %>%
     filter(complete.cases(.)) %>%
     select(-datazone, -period) %>%
-    princomp(., cor=T)
-
-summary(fit)
-plot(fit)
+    prcomp(., cor=T)
 
 
-pairs(fit$loadings[,1:5])
 
-pairs(fit$loadings[,1:5], col=1:(ncol(p_all_H) - 2), upper.panel=NULL, main="Factor loadings")
-par(xpd=TRUE) 
-legend('topright', bty='n', pch='o', col=1:(ncol(p_all_H) - 2), attr(fit$loadings, 'dimnames')[[1]], title="Variables")
+# table of S loadings around 2001 -----------------------------------------
 
-m1 <- p_all_H  %>% 
-    filter(period=="t1")  %>% 
+fit_t1$rotation  %>% 
+    round(2)  %>%
+    xtable %>%
+    print(type="html", file="tables/shannon_factor_loadings_t1.html")
+
+
+# Graph of S loadings around 2001 -----------------------------------------
+
+fit_t1$rotation  %>% 
+    round(2)  %>% 
+    as.data.frame  %>%
+    mutate(type=rownames(.))  %>% 
+    gather(key="pc", value="loading", -type)  %>% 
+    ggplot(data=.) + 
+    geom_point(aes(x=loading, y= type)) + 
+    facet_wrap( ~ pc) + 
+    geom_segment(
+        aes(
+            y=type, yend=type, 
+            x=ifelse(loading < 0, loading, 0), 
+            xend=ifelse(loading > 0, loading, 0)
+        )
+    ) + 
+    labs(x="Factor loading on Principal Component", y="Entropy Type", title="Factor loadings around 2001") +
+    theme(plot.title=element_text(face = "bold"))
+ggsave("figures/S_factor_loadings_pca_around_2001.tiff", dpi=300, height=30, width=30, unit="cm")
+
+
+
+# pca around 2011 ---------------------------------------------------------
+
+fit_t2 <- p_all_S %>%
+    filter(period=="t2") %>%
     filter(complete.cases(.)) %>%
-    select(-datazone, -period)  %>% 
-    factanal(
-    x=. , 
-    factors=1
-    )
-
-m2 <- p_all_H  %>% 
-    filter(period=="t1")  %>% 
-    filter(complete.cases(.)) %>%
-    select(-datazone, -period)  %>% 
-    factanal(
-        x=. , 
-        factors=2
-    )
-
-m3 <- p_all_H  %>% 
-    filter(period=="t1")  %>% 
-    filter(complete.cases(.)) %>%
-    select(-datazone, -period)  %>% 
-    factanal(
-        x=. , 
-        factors=3
-    )
-
-m4 <- p_all_H  %>% 
-    filter(period=="t1")  %>% 
-    filter(complete.cases(.)) %>%
-    select(-datazone, -period)  %>% 
-    factanal(
-        x=. , 
-        factors=4
-    )
+    select(-datazone, -period) %>%
+    prcomp(., cor=T)
 
 
 
-load2 <- m2$loadings
-plot(load2, type="n", xlim=c(-1, 1), ylim=c(-1,1), 
-    xlab="Loading on first factor", ylab="Loading on second factor", 
-    main="Diversity Correlations: Two factor model") 
-text(load2, labels=names(p_all_H), cex=0.7) 
-abline(v=0, lty="dashed"); abline(h=0, lty="dashed")
+# table of S loadings around 2011 -----------------------------------------
 
-load1_2 <- m3$loadings[,1:2]
-plot(load1_2, type="n", xlim=c(-1, 1), ylim=c(-1,1), 
-     xlab="Loading on first factor", ylab="Loading on second factor", 
-     main="Diversity Correlations: Three factor model") 
-text(load3_1, labels=names(p_all_H), cex=0.7) 
-abline(v=0, lty="dashed"); abline(h=0, lty="dashed")
 
-load2_3 <- m3$loadings[,2:3]
-plot(load2_3, type="n", xlim=c(-1, 1), ylim=c(-1,1), 
-     xlab="Loading on second factor", ylab="Loading on third factor", 
-     main="Diversity Correlations: Three factor model") 
-text(load3_2, labels=names(p_all_H), cex=0.7) 
-abline(v=0, lty="dashed"); abline(h=0, lty="dashed")
+fit_t2$rotation  %>% 
+    round(2)  %>%
+    xtable %>%
+    print(type="html", file="tables/S_factor_loadings_t2.html")
 
-load1_3 <- m3$loadings[,c(1, 3)]
-plot(load1_3, type="n", xlim=c(-1, 1), ylim=c(-1,1), 
-     xlab="Loading on first factor", ylab="Loading on third factor", 
-     main="Diversity Correlations: Three factor model") 
-text(load1_3, labels=names(p_all_H), cex=0.7) 
-abline(v=0, lty="dashed"); abline(h=0, lty="dashed")
+# Graph of H loadings around 2001 -----------------------------------------
 
-# 3d representation of factors
-tmp <- m3$loadings[,1:3]
-
-open3d()
-decorate3d(
-    xlim=c(-1, 1), ylim=c(-1, 1), zlim=c(-1, 1),
-    xlab="F1", ylab="F2", zlab="F3", box=FALSE, axes=FALSE
-)
-text3d(
-    x=tmp[,1], y=tmp[,2], z=tmp[,3],
-    text=rownames(tmp)
-)
-
-planes3d(a=1, b=0, c=0, alpha=0.2, col="red")
-planes3d(a=0, b=1, c=0, alpha=0.2, col="green")
-planes3d(a=0, b=0, c=1, alpha=0.2, col="blue")
-points3d(x=tmp[,1], y=tmp[,2], z=tmp[,3], size=3)
+fit_t2$rotation  %>% 
+    round(2)  %>% 
+    as.data.frame  %>%
+    mutate(type=rownames(.))  %>% 
+    gather(key="pc", value="loading", -type)  %>% 
+    ggplot(data=.) + 
+    geom_point(aes(x=loading, y= type)) + 
+    facet_wrap( ~ pc) + 
+    geom_segment(
+        aes(
+            y=type, yend=type, 
+            x=ifelse(loading < 0, loading, 0), 
+            xend=ifelse(loading > 0, loading, 0)
+        )
+    ) + 
+    labs(x="Factor loading on Principal Component", y="Entropy Type", title="Factor loadings around 2011") +
+    theme(plot.title=element_text(face = "bold"))
+ggsave("figures/s_factor_loadings_pca_around_2011.tiff", dpi=300, height=30, width=30, unit="cm")
